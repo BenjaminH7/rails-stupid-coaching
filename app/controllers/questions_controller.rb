ff760776a3553300b229a851c5d23e24197d1c7d
Benjamin Heymann <benjamin@bretler.ch>
2024-08-20 18:07:54 +0200

    ok

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,18 +4,13 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @answer = coach_answer(@question)
-  end
 
-
-
-  def coach_answer(question)
-    if question == 'i am going to work'
-      'Great!'
-    elsif question.end_with?('?')
-      'Silly question, get dressed and go to work!'
+    if @question === 'i am going to work'
+      @answer = 'Great!'
+    elsif @question.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
     else
-      "I don't care, get dressed and go to work!"
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
